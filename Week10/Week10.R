0c0be7ba42e5bdcab94b1e00cf8bfa2ff6c8d6b9
cjower6790 <32119089+cjower6790@users.noreply.github.com>
2017-11-04 19:59:59 -0700

    First part of portfolio
    Transposed data
    Examined NativeVsWeed relative frequency by height class

---
+++ Week10/Week10.R
@@ -0,0 +1,10 @@
+install.packages("RColorBrewer")
+install.packages("viridis")
+install.packages("wesanderson")
+plot(x=1, y = 1, col = rgb(166,206,227, maxColorValue = 255), type = 'p', pch = 17, cex = 6)
+points(x=1.01, y=1.02, pch = 17, cex = 6, col = rgb (0,1,0,0.5))
+points(x=1.02, y = 1.03, pch = 17, cex = 6, col = rgb (1,0,0,0.5))
+
+# Week 11
+
+For example, this set of options tells the 'knit'
